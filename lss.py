51b5810a57d5dd6c02119c70f1031441ad94db0e
Christopher Goulet <christophergoulet@outlook.com>
2017-07-11 11:07:54 -0500

    Create lss.py

---
+++ lss.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# Get-ChildItem/dir/ls is horrible in powershell,  here is a quick and dirty script to list files in a more sane way
+# Best used if you create an function and put it in your profile
+# Ex:
+# notepad.exe $profile
+# add the following line
+# function lss {python c:\scripts\lss.py}
+
+import os
+import subprocess
+
+height = int(subprocess.check_output("powershell.exe -NoProfile (get-host).UI.RawUI.WindowSize.Height", stderr=subprocess.STDOUT, shell=True)[:-2])
+#width = int(subprocess.check_output("powershell.exe -NoProfile (get-host).UI.RawUI.WindowSize.Width", stderr=subprocess.STDOUT, shell=True)[:-2])
+
+files = os.listdir("./")
+
+os.system("clear")
+
+i = 0
+while i<len(files):
+    print(files[i])
+    if((i+1)%(height-1)==0 and i!=0):
+        if(input("-- More --") == 'b'):
+            i-=((height-1)*2)
+            if(i<0):
+                i=0
+                os.system("clear")
+                continue
+        os.system("clear")
+    i += 1
